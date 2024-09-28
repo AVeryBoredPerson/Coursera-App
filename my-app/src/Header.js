aea5ea7dd09b68675c252be6c41e1f486afd43c7
unknown <Meyertravis7@gmail.com>
2024-09-28 15:40:13 -0700

    Commitiing

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -2,10 +2,6 @@ export default function Header() {
     return (
         <header style={{display: inline-flex}}>
             <img src="Logo.svg"></img>
-            <meta name="description" content="The Little Lemon restaurant presents fresh and delightful meals every singel day."></meta>
-            <meta name="og:title" content="Little Lemon"></meta>
-            <meta name="og:description" content="The Little Lemon restaurant presents fresh and delightful meals every singel day."></meta>
-            <meta name="og:image" content="Logo.svg"></meta>
         </header>
     );
 }
